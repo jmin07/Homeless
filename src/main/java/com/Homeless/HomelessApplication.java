54cf8a2d8d51f8e3b7cf83298d3de8f527463aa1
Jong Min <jmbaek07@gmail.com>
2022-12-03 17:07:00 +0900

    초기 init

---
+++ src/main/java/com/Homeless/HomelessApplication.java
@@ -0,0 +1,13 @@
+package com.Homeless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomelessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomelessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Homeless/HomelessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Homeless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
