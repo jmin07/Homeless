fe81b825c96817549f05736c594da997615f0b0f
Jong Min <jmbaek07@gmail.com>
2022-12-13 01:41:23 +0900

    [초기 설정] 회원 로그인(Member) 및 프론트 템플릿

--- src/main/java/com/Homeless/HomelessApplication.java
+++ src/main/java/com/Homeless/HomelessApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HomelessApplication {
 
 	public static void main(String[] args) {
-
-		System.out.println("깃 pust test"); //12.3 17:41 규민
-		System.out.println("종민 테스트");
 		SpringApplication.run(HomelessApplication.class, args);
 	}
 
